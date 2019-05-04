88048ef7f70e826cb8d864bef9bd78926d07514f
snamiki1212 <snamiki1212@gmail.com>
2019-05-04 23:44:48 +0700

    Updates

---
+++ precache-manifest.0fd24af66cbf8415ee5acbedeef93189.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4151a21664a092710d97b4d15d3bc315",
+    "url": "/snamiki1212/milkmemo/index.html"
+  },
+  {
+    "revision": "c0021eccc3a4f1661811",
+    "url": "/snamiki1212/milkmemo/static/css/main.3e545749.chunk.css"
+  },
+  {
+    "revision": "e2c46710392b2f2af3f0",
+    "url": "/snamiki1212/milkmemo/static/js/2.5151c6ca.chunk.js"
+  },
+  {
+    "revision": "c0021eccc3a4f1661811",
+    "url": "/snamiki1212/milkmemo/static/js/main.d32424f9.chunk.js"
+  },
+  {
+    "revision": "0dd63fdb2ca285098ccd",
+    "url": "/snamiki1212/milkmemo/static/js/runtime~main.2b5537e2.js"
+  }
+]);
