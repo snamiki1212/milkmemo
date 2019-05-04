7f7b9bb037caa81f104bb24891e49c05aa5c24b7
snamiki1212 <snamiki1212@gmail.com>
2019-05-04 23:49:29 +0700

    Updates

---
+++ precache-manifest.2bb681f7d6f43590565284f3fef255a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eeea1f60165eb542223ce8ebc7780fe",
+    "url": "/milkmemo/index.html"
+  },
+  {
+    "revision": "087223cf35c643f8a67d",
+    "url": "/milkmemo/static/css/main.3e545749.chunk.css"
+  },
+  {
+    "revision": "e2c46710392b2f2af3f0",
+    "url": "/milkmemo/static/js/2.5151c6ca.chunk.js"
+  },
+  {
+    "revision": "087223cf35c643f8a67d",
+    "url": "/milkmemo/static/js/main.8fb7ecc8.chunk.js"
+  },
+  {
+    "revision": "2334fab7b51d23c842b3",
+    "url": "/milkmemo/static/js/runtime~main.69b6bb19.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/snamiki1212/milkmemo/precache-manifest.0fd24af66cbf8415ee5acbedeef93189.js"
+  "/milkmemo/precache-manifest.2bb681f7d6f43590565284f3fef255a9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/snamiki1212/milkmemo/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/milkmemo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
